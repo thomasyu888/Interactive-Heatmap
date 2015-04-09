7f93257bcbb04ce100dda839425543014c817288
Thomas Yu <thomasyu888@gmail.com>
2015-04-09 11:25:00 -0700

    Update Clustering.R

--- Heatmap/Clustering.R
+++ Heatmap/Clustering.R
@@ -26,25 +26,35 @@ HCtoJSON<-function(hc){
 }
 
 
-genes <- read.csv(file="INPUT MAIN DATA",head=TRUE,row.names=1)
+genes <- read.csv(file="/Users/thomasyu/Desktop/Sage Bionetworks/Heatmap_FINAL_Alljson/PCBC_geneExpr_data.csv",head=TRUE,row.names=1)
 gene <- as.matrix(genes)
-metadatas <- read.csv(file="INPUT METADATA",head=TRUE,row.names=1)
+metadatas <- read.csv(file="/Users/thomasyu/Desktop/Sage Bionetworks/Heatmap_FINAL_Alljson/metadata.csv",head=TRUE,row.names=1)
 metadata <- as.matrix(metadatas)
-
-
 
 rowClust <- hclust(dist(gene))
 gene <- gene[rowClust$order,]
 
 colClust <- hclust(dist(t(gene)))
 gene <- gene[,colClust$order]
-
 metadata <- metadata[colClust$order,]
+
+rows <- row.names(gene)
+scale <- range(gene)
+cols <- colnames(gene)
+domain <- seq.int(ceiling(scale[2]),floor(scale[1]), length.out = 100)
+colors <- heat.colors(100)
 
 rowDend <- HCtoJSON(rowClust)
 colDend <- HCtoJSON(colClust)
 writeLines(rowDend, "rowDendro.json")
 writeLines(colDend, "colDendro.json")
-write.csv(gene, "gene.csv")
-write.csv(metadata, "metadata.csv")
+writeLines(toJSON(metadata), "metadata.json")
+total <- toJSON(list(gene,
+                     rows,
+                     cols,
+                     domain,
+                     sub('FF$', '', colors)))
+
+writeLines(total,"total.json")
+
 
